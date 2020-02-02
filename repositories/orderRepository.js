7199ba8a1cf56ae6009de9dd985e500722b99ba7
unknown <300755@uwr.edu.pl>
2020-02-02 02:26:19 +0100

    date change

--- repositories/orderRepository.js
+++ repositories/orderRepository.js
@@ -4,20 +4,20 @@ const { Op } = require("sequelize");
 
 class OrderRepository {
 
-    async getAllOrders(){
-        const orders = await models.Order.findAll({include: [{model: models.Item, as:'items'}]});
+    async getAllOrders() {
+        const orders = await models.Order.findAll({ include: [{ model: models.Item, as: 'items' }] });
         return orders;
     }
 
-    async getOrderById(id){
-        return await models.Order.findOne({include: [{model: model.Item, as: 'items'}], where: {id: id}});
+    async getOrderById(id) {
+        return await models.Order.findOne({ include: [{ model: model.Item, as: 'items' }], where: { id: id } });
     }
 
-    async makeNewOrder(userEmail, order){
+    async makeNewOrder(userEmail, order) {
         const user = await userRepository.getUserByEmail(userEmail);
         let items = await order.getOrder();
         let total = order.getTotal();
-        const new_order = await models.Order.create({ userId: user.id, total: total, date: new Date() });
+        const new_order = await models.Order.create({ userId: user.id, total: total, date: new Date().toLocaleString() });
         for (let i = 0; i < items.length; i++) {
             let item = await models.OrdersItems.create({ orderId: new_order.id, itemId: items[i].id, quantity: items[i].quantity, total: items[i].itemTotal });
             console.log(item);
